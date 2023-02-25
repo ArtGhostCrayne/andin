ecdf4c9dc7988c62543488bc93f20a6cfef41e78
ArtGhost <artghostcrayne@mail.ru>
2023-02-25 19:54:04 +0300

    07_crud

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,36 +1,39 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun getAll(): List<Post>
+//    fun getAll(): List<Post>
 //    fun likeById(id: Long): List<Post>
 
-    fun getAllAsync(callback: GetAllCallback)
+    fun getAllAsync(callback: GetAllCallback<List<Post>>)
+
+    interface GetAllCallback<T> {
+        fun onSuccess(posts: T) {}
+        fun onError(e: Exception) {}
+    }
 
 
 //    fun likeById(id: Long): Post
-    fun likeById(id: Long, callback: GetLikeCallback)
+    fun likeById(id: Long, callback: GetCallback<Post>)
+
+    interface GetLikeCallback<T> {
+        fun onSuccess(post: T) {}
+        fun onError(e: Exception) {}
+    }
 
 //    fun unlikeById(id: Long): Post
-    fun unlikeById(id: Long, callback: GetLikeCallback)
-    fun save(post: Post, callback: GetCallback)
+    fun unlikeById(id: Long, callback: GetCallback<Post>)
+    fun save(post: Post, callback: GetCallback<Post>)
 //    fun removeById(id: Long)
-    fun removeById(id: Long, callback: GetCallback)
+    fun removeById(id: Long, callback: GetCallback<Unit>)
 
-    interface GetAllCallback {
-        fun onSuccess(posts: List<Post>) {}
+
+
+    interface GetCallback<T> {
+        fun onSuccess(post: T) {}
         fun onError(e: Exception) {}
     }
 
-    interface GetCallback {
-        fun onSuccess() {}
-        fun onError(e: Exception) {}
-    }
 
-    interface GetLikeCallback {
-        fun onSuccess(post: Post) {}
-        fun onError(e: Exception) {}
-    }
 }
